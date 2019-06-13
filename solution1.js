cfd44c3152b89aa2f06b058d401a8d433f6d0154
kapi1Pandey <41443779+kapi1Pandey@users.noreply.github.com>
2019-06-13 16:31:31 +0530

    Update solution1.js

--- solution1.js
+++ solution1.js
@@ -1,4 +1,9 @@
- var arr=new Array(20);
+/*  Question
+1. You will write a script which will alloate array of 20 integers 
+making sure that we initializes each element by its index and multiplied by 5 . Print the output in the console
+*/
+
+var arr=new Array(20);
 //method 1
 for(let i=0;i<20;i++)
 {
