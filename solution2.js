a2af8059eacdda2666a9972b6cc1b3a7ca08d827
kapi1Pandey <41443779+kapi1Pandey@users.noreply.github.com>
2019-06-17 12:21:05 +0530

    Update solution2.js

--- solution2.js
+++ solution2.js
@@ -6,26 +6,6 @@
 var str1="kapil";
 var str2="pandey";
 
-var char1=Array.from(str1)
-var char2=Array.from(str2)
-
-var flag=true;
-if(char1.length!=char2.length)
-console.log("character arrays are not equal")
-else{
-    char1.sort();
-    char2.sort();
-    for(let i=0;i<char1.length;i++)
-    {
-        if(char1[i]!=char2[i])
-        {
-             console.log("Both have different elements")
-             flag=false;
-            break;
-        }
-    }
-    if(flag)
-    {
-        console.log("Both have same elements")
-    }
-}
+var str1="kapil";
+var str2="kapil";
+console.log((str1.localeCompare(str2)==-1||str1.localeCompare(str2)==1)?"Not equal":"Equal")
