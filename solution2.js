5a41473b60d2bd14f8aac6ae53957683f8c9f2cd
kapi1Pandey <41443779+kapi1Pandey@users.noreply.github.com>
2019-06-17 09:49:51 +0530

    Update solution2.js

--- solution2.js
+++ solution2.js
@@ -1,11 +1,5 @@
 var arr = [1, 2, 1, 4, 1, 3, 4, 1, 111, 3, 2, 1, '1'];
 Array.prototype.remove = function (value) {
-    // arrRemoved = [];
-    // for (let i = 0; i < this.length; i++) {
-    //     if (this[i] !== value)
-    //         arrRemoved.push(this[i])
-    // }
-    // return arrRemoved;
     return this.filter(function(index){
         return index !== value;
     })
