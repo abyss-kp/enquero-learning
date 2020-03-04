83444f327f34933e92936caa8e4c3c8fe9df4b13
kapil_pandey😎 <abyss6271@gmail.com>
2020-03-04 11:32:22 +0530

    fully functional pollyfill

--- Interview/Pollyfill_for_Bind.js
+++ Interview/Pollyfill_for_Bind.js
@@ -3,16 +3,16 @@ let object = {
   lName: "Pandey"
 }
 
-let print = function () {
-  console.log(`My name is ${this.fName}`)
+let print = function (home, dist, state) {
+  console.log(`My name is ${this.fName} and I am from ${home} ${dist} ${state}`)
 }
 // print.bind(object)()
 Function.prototype.myBind = function (...args) {
   let obj = this
-  return function () {
-    obj.call(args[0])
+  return function (...args2) {
+    obj.apply(args[0], [...args2, ...args.slice(1)])
   }
 }
 
-let printMyName = print.myBind(object)
+let printMyName = print.myBind(object, "Bhimtal", "Naintal", "Uttrakhand")
 printMyName()
