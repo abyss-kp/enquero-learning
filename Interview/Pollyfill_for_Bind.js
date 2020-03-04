086de2c91281018d8eb0732db789d402c31ec6f3
kapil_pandey😎 <abyss6271@gmail.com>
2020-03-04 11:27:06 +0530

    created pollyfill

--- Interview/Pollyfill_for_Bind.js
+++ Interview/Pollyfill_for_Bind.js
@@ -6,5 +6,13 @@ let object = {
 let print = function () {
   console.log(`My name is ${this.fName}`)
 }
-print.bind(object)()
+// print.bind(object)()
+Function.prototype.myBind = function (...args) {
+  let obj = this
+  return function () {
+    obj.call(args[0])
+  }
+}
 
+let printMyName = print.myBind(object)
+printMyName()
